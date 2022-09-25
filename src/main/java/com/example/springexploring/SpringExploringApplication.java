b07695f8a2fdb52cf99d0a2c65ab184d1cfeac07
Nicolae Gherta <ngherta@inthergroup.com>
2022-09-25 19:05:41 +0300

    first commit

---
+++ src/main/java/com/example/springexploring/SpringExploringApplication.java
@@ -0,0 +1,13 @@
+package com.example.springexploring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExploringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringExploringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springexploring/SpringExploringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springexploring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExploringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
